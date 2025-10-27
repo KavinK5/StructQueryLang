3a55432cf19e0c3e77a2fc4cf8df8201425a6810
KavinK5 <krkavin346@gmail.com>
2025-10-27 18:24:22 +0530

    feat: 4 Important SQL Constraints Added [2ways]

--- ConstraintsDecoded_SQL.sql
+++ ConstraintsDecoded_SQL.sql
@@ -13,10 +13,72 @@ DIFFERENT WAYS TO ADD CONSTRAINTS
 """
 
 
--- Adding NotNULL Constraint
+-- Adding NOT NULL Constraint
 CREATE TABLE EmployeeSalaryTable(
     EmployeeID VARCHAR NOT NULL,
     EmployeeName VARCHAR NOT NULL,
     Department VARCHAR,
     Salary BIGINT
 )
+
+-- Adding DEFAULT Constraint
+CREATE TABLE BarclaysStakeHolders(
+    EmployeeID   VARCHAR,
+    Gender_ VARCHAR DEFAULT 'NonSpecified',
+    EmployeeName VARCHAR
+)
+
+
+-- Adding UNIQUE Constraint #1
+CREATE TABLE EmployeeTable(
+    EmployeeID VARCHAR NOT NULL,
+    EmployeeName VARCHAR NOT NULL,
+    Department VARCHAR,
+    UNIQUE (EmployeeID)
+)
+
+-- Adding UNIQUE Constraint #2
+CREATE TABLE EmployeeTable_(
+    EmployeeID VARCHAR NOT NULL,
+    EmployeeName VARCHAR NOT NULL,
+    Department VARCHAR,
+    UNIQUE (EmployeeName, Department)
+)
+
+
+-- Adding CHECK Constraint
+CREATE TABLE BarclaysPublicInvestors(
+    Fundraiser VARCHAR,
+    DepositAmount BIGINT,
+    createdAt TIMESTAMP,
+    CHECK ( 
+            (TRIM(Fundraiser) != '')    AND 
+            (Fundraiser IS NOT NULL)    AND 
+            (DepositAmount >= 1500000) 
+        )
+)
+
+
+
+--ALTER COMMANDS
+ALTER TABLE EmployeeSalaryTable
+ALTER COLUMN Department
+SET DEFAULT  NULL
+
+
+
+ALTER TABLE BarclaysStakeHolders
+ALTER COLUMN EmployeeID
+SET NOT NULL
+
+
+
+ALTER TABLE EmployeeSalaryTable
+ADD CONSTRAINT SalaryCheckConstraint
+CHECK  (Salary > 0)
+
+
+
+ALTER TABLE BarclaysStakeHolders
+ADD CONSTRAINT EmpIDUnique_Constraint
+UNIQUE (EmployeeID)
