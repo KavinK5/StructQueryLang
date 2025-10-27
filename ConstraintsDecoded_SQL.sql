0bf0c1c5b67a8ec36496fee515a814bf3cb31188
KavinK5 <krkavin346@gmail.com>
2025-10-27 15:59:33 +0530

    feat: Added decription

---
+++ ConstraintsDecoded_SQL.sql
@@ -0,0 +1,15 @@
+"""
+CONSTRAINTS IN StructQueryLang
+
+    1. Assigning the COLUMN that it should never take NULLs
+    2. Assigning DEFAULT value to a COLUMN of its specified datatype
+    3. The COLUMN must hold UNIQUE values
+    4. CHECK: Performs Criteria Check
+"""
+
+"""
+DIFFERENT WAYS TO ADD CONSTRAINTS
+
+    1. While Table CREATE
+    2. After Table Creation, Table ALTER  
+"""
